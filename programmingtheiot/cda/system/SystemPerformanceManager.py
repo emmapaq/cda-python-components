d497769d52582133fc1938e4cee3b33c316b9b2e
Emma Frimpong <emmapaq@gmail.com>
2025-10-12 21:36:45 -0400

    Lab Module 05 - Data Integration

--- programmingtheiot/cda/system/SystemPerformanceManager.py
+++ programmingtheiot/cda/system/SystemPerformanceManager.py
@@ -1,21 +1,7 @@
-#####
-# 
-# This class is part of the Programming the Internet of Things
-# project, and is available via the MIT License, which can be
-# found in the LICENSE file at the top level of this repository.
-# 
-# You may find it more helpful to your design to adjust the
-# functionality, constants and interfaces (if there are any)
-# provided within in order to meet the needs of your specific
-# Programming the Internet of Things project.
-# 
-
 import logging
-
 from apscheduler.schedulers.background import BackgroundScheduler
 
 import programmingtheiot.common.ConfigConst as ConfigConst
-
 from programmingtheiot.common.ConfigUtil import ConfigUtil
 from programmingtheiot.common.IDataMessageListener import IDataMessageListener
 
@@ -24,23 +10,67 @@ from programmingtheiot.cda.system.SystemMemUtilTask import SystemMemUtilTask
 
 from programmingtheiot.data.SystemPerformanceData import SystemPerformanceData
 
-class SystemPerformanceManager(object):
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+class SystemPerformanceManager:
+    """
+    SystemPerformanceManager collects system performance metrics
+    such as CPU and memory utilization, stores them in
+    SystemPerformanceData, and notifies a listener if set.
+    """
 
-	def __init__(self):
-		pass
+    def __init__(self, locationID: str = None, pollRate: int = 5):
+        # Initialize tasks
+        self.cpuUtilTask = SystemCpuUtilTask()
+        self.memUtilTask = SystemMemUtilTask()
+        
+        # Telemetry values
+        self.cpuUtilPct = 0.0
+        self.memUtilPct = 0.0
+        
+        # Optional location ID for system
+        self.locationID = locationID
+        
+        # Listener for callbacks
+        self.dataMsgListener: IDataMessageListener | None = None
+        
+        # Scheduler for periodic telemetry collection
+        self.scheduler = BackgroundScheduler()
+        self.pollRate = pollRate  # seconds
 
-	def handleTelemetry(self):
-		pass
-		
-	def setDataMessageListener(self, listener: IDataMessageListener) -> bool:
-		pass
-	
-	def startManager(self):
-		pass
-		
-	def stopManager(self):
-		pass
+    def handleTelemetry(self):
+        """Collect CPU and memory usage and notify listener if set."""
+        self.cpuUtilPct = self.cpuUtilTask.getTelemetryValue()
+        self.memUtilPct = self.memUtilTask.getTelemetryValue()
+        
+        logging.debug(
+            "CPU utilization: %s%%, Memory utilization: %s%%",
+            self.cpuUtilPct, self.memUtilPct
+        )
+        
+        # Create SystemPerformanceData object
+        sysPerfData = SystemPerformanceData()
+        sysPerfData.setLocationID(self.locationID)
+        sysPerfData.setCpuUtilization(self.cpuUtilPct)
+        sysPerfData.setMemoryUtilization(self.memUtilPct)
+        
+        # Notify listener
+        if self.dataMsgListener:
+            self.dataMsgListener.handleSystemPerformanceMessage(data=sysPerfData)
+
+    def setDataMessageListener(self, listener: IDataMessageListener) -> bool:
+        """Set the listener for telemetry callbacks."""
+        if listener:
+            self.dataMsgListener = listener
+            return True
+        return False
+
+    def startManager(self):
+        """Start periodic telemetry collection."""
+        self.scheduler.add_job(self.handleTelemetry, 'interval', seconds=self.pollRate)
+        self.scheduler.start()
+        logging.info("SystemPerformanceManager started with poll rate %s seconds.", self.pollRate)
+
+    def stopManager(self):
+        """Stop periodic telemetry collection."""
+        if self.scheduler.running:
+            self.scheduler.shutdown(wait=False)
+            logging.info("SystemPerformanceManager stopped.")
--- programmingtheiot/data/DataUtil.py
+++ programmingtheiot/data/DataUtil.py
@@ -1,54 +1,112 @@
-#####
-# 
-# This class is part of the Programming the Internet of Things
-# project, and is available via the MIT License, which can be
-# found in the LICENSE file at the top level of this repository.
-# 
-# You may find it more helpful to your design to adjust the
-# functionality, constants and interfaces (if there are any)
-# provided within in order to meet the needs of your specific
-# Programming the Internet of Things project.
-# 
-
+import json
+import logging
+from decimal import Decimal
 from json import JSONEncoder
 
 from programmingtheiot.data.ActuatorData import ActuatorData
 from programmingtheiot.data.SensorData import SensorData
 from programmingtheiot.data.SystemPerformanceData import SystemPerformanceData
 
-class DataUtil():
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+class DataUtil:
+    def __init__(self, encodeToUtf8: bool = False):
+        self.encodeToUtf8 = encodeToUtf8
+        logging.info("Created DataUtil instance.")
 
-	def __init__(self, encodeToUtf8 = False):
-		pass
-	
-	def actuatorDataToJson(self, data: ActuatorData = None):
-		pass
-	
-	def sensorDataToJson(self, data: SensorData = None):
-		pass
+    # ----------------------
+    # ActuatorData Conversions
+    # ----------------------
+    def actuatorDataToJson(self, data: ActuatorData = None, useDecForFloat: bool = False) -> str:
+        if not data:
+            logging.debug("ActuatorData is null. Returning empty string.")
+            return ""
+        return self._generateJsonData(obj=data, useDecForFloat=useDecForFloat)
 
-	def systemPerformanceDataToJson(self, data: SystemPerformanceData = None):
-		pass
-	
-	def jsonToActuatorData(self, jsonData: str = None):
-		pass
-	
-	def jsonToSensorData(self, jsonData: str = None):
-		pass
-	
-	def jsonToSystemPerformanceData(self, jsonData: str = None):
-		pass
-	
+    def jsonToActuatorData(self, jsonData: str = None, useDecForFloat: bool = False) -> ActuatorData | None:
+        if not jsonData:
+            logging.warning("JSON data is empty or null. Returning None.")
+            return None
+        jsonStruct = self._formatDataAndLoadDictionary(jsonData, useDecForFloat)
+        ad = ActuatorData()
+        self._updateIotData(jsonStruct, ad)
+        return ad
+
+    # ----------------------
+    # SensorData Conversions
+    # ----------------------
+    def sensorDataToJson(self, data: SensorData = None, useDecForFloat: bool = False) -> str:
+        if not data:
+            logging.debug("SensorData is null. Returning empty string.")
+            return ""
+        return self._generateJsonData(obj=data, useDecForFloat=useDecForFloat)
+
+    def jsonToSensorData(self, jsonData: str = None, useDecForFloat: bool = False) -> SensorData | None:
+        if not jsonData:
+            logging.warning("JSON data is empty or null. Returning None.")
+            return None
+        jsonStruct = self._formatDataAndLoadDictionary(jsonData, useDecForFloat)
+        sd = SensorData()
+        self._updateIotData(jsonStruct, sd)
+        return sd
+
+    # ----------------------
+    # SystemPerformanceData Conversions
+    # ----------------------
+    def systemPerformanceDataToJson(self, data: SystemPerformanceData = None, useDecForFloat: bool = False) -> str:
+        if not data:
+            logging.debug("SystemPerformanceData is null. Returning empty string.")
+            return ""
+        return self._generateJsonData(obj=data, useDecForFloat=useDecForFloat)
+
+    def jsonToSystemPerformanceData(self, jsonData: str = None, useDecForFloat: bool = False) -> SystemPerformanceData | None:
+        if not jsonData:
+            logging.warning("JSON data is empty or null. Returning None.")
+            return None
+        jsonStruct = self._formatDataAndLoadDictionary(jsonData, useDecForFloat)
+        spd = SystemPerformanceData()
+        self._updateIotData(jsonStruct, spd)
+        return spd
+
+    # ----------------------
+    # Private helper methods
+    # ----------------------
+    def _formatDataAndLoadDictionary(self, jsonData: str, useDecForFloat: bool = False) -> dict:
+        """
+        Prepare JSON string and load into a dictionary.
+        """
+        jsonData = jsonData.replace("'", '"').replace('False', 'false').replace('True', 'true')
+        if useDecForFloat:
+            return json.loads(jsonData, parse_float=Decimal)
+        return json.loads(jsonData)
+
+    def _generateJsonData(self, obj, useDecForFloat: bool = False) -> str:
+        """
+        Convert an object to a JSON string using JsonDataEncoder.
+        """
+        if self.encodeToUtf8:
+            jsonData = json.dumps(obj, cls=JsonDataEncoder).encode('utf8')
+        else:
+            jsonData = json.dumps(obj, cls=JsonDataEncoder, indent=4)
+
+        if jsonData:
+            jsonData = jsonData.replace("'", '"').replace('False', 'false').replace('True', 'true')
+        return jsonData
+
+    def _updateIotData(self, jsonStruct: dict, obj):
+        """
+        Map dictionary values into the object's attributes.
+        """
+        varStruct = vars(obj)
+        for key in jsonStruct:
+            if key in varStruct:
+                setattr(obj, key, jsonStruct[key])
+            else:
+                logging.warning("JSON data contains key not mappable to object: %s", key)
+
+
 class JsonDataEncoder(JSONEncoder):
-	"""
-	Convenience class to facilitate JSON encoding of an object that
-	can be converted to a dict.
-	
-	"""
-	def default(self, o):
-		return o.__dict__
-	
+    """
+    Convenience class to facilitate JSON encoding of an object that
+    can be converted to a dict.
+    """
+    def default(self, o):
+        return o.__dict__
