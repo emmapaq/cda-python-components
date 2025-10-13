83316b7c451e5995ae10868e45ff6c81a1814d7b
Emma Frimpong <emmapaq@gmail.com>
2025-10-13 17:39:09 -0400

    Lab Module 05 Final

--- programmingtheiot/data/DataUtil.py
+++ programmingtheiot/data/DataUtil.py
@@ -1,54 +1,93 @@
-#####
-# 
-# This class is part of the Programming the Internet of Things
-# project, and is available via the MIT License, which can be
-# found in the LICENSE file at the top level of this repository.
-# 
-# You may find it more helpful to your design to adjust the
-# functionality, constants and interfaces (if there are any)
-# provided within in order to meet the needs of your specific
-# Programming the Internet of Things project.
-# 
-
-from json import JSONEncoder
+import json
+import logging
 
 from programmingtheiot.data.ActuatorData import ActuatorData
 from programmingtheiot.data.SensorData import SensorData
 from programmingtheiot.data.SystemPerformanceData import SystemPerformanceData
 
-class DataUtil():
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+class DataUtil:
+    """
+    Utility class for serializing and deserializing IoT data objects to/from JSON.
+    """
 
-	def __init__(self, encodeToUtf8 = False):
-		pass
-	
-	def actuatorDataToJson(self, data: ActuatorData = None):
-		pass
-	
-	def sensorDataToJson(self, data: SensorData = None):
-		pass
+    def __init__(self, encodeToUtf8: bool = False):
+        self.encodeToUtf8 = encodeToUtf8
+        self.logger = logging.getLogger("DataUtil")
 
-	def systemPerformanceDataToJson(self, data: SystemPerformanceData = None):
-		pass
-	
-	def jsonToActuatorData(self, jsonData: str = None):
-		pass
-	
-	def jsonToSensorData(self, jsonData: str = None):
-		pass
-	
-	def jsonToSystemPerformanceData(self, jsonData: str = None):
-		pass
-	
-class JsonDataEncoder(JSONEncoder):
-	"""
-	Convenience class to facilitate JSON encoding of an object that
-	can be converted to a dict.
-	
-	"""
-	def default(self, o):
-		return o.__dict__
-	
+    # ---- ActuatorData conversions ----
+    def actuatorDataToJson(self, actuatorData: ActuatorData) -> str:
+        if actuatorData is None:
+            return None
+        try:
+            jsonData = json.dumps(actuatorData.__dict__)
+            return jsonData.encode("utf-8") if self.encodeToUtf8 else jsonData
+        except Exception as e:
+            self.logger.error("Failed to convert ActuatorData to JSON: %s", e)
+            return None
+
+    def jsonToActuatorData(self, jsonData: str) -> ActuatorData:
+        if not jsonData:
+            return None
+        try:
+            if self.encodeToUtf8 and isinstance(jsonData, bytes):
+                jsonData = jsonData.decode("utf-8")
+
+            dataDict = json.loads(jsonData)
+            ad = ActuatorData()
+            ad.__dict__.update(dataDict)
+            return ad
+        except Exception as e:
+            self.logger.error("Failed to convert JSON to ActuatorData: %s", e)
+            return None
+
+    # ---- SensorData conversions ----
+    def sensorDataToJson(self, sensorData: SensorData) -> str:
+        if sensorData is None:
+            return None
+        try:
+            jsonData = json.dumps(sensorData.__dict__)
+            return jsonData.encode("utf-8") if self.encodeToUtf8 else jsonData
+        except Exception as e:
+            self.logger.error("Failed to convert SensorData to JSON: %s", e)
+            return None
+
+    def jsonToSensorData(self, jsonData: str) -> SensorData:
+        if not jsonData:
+            return None
+        try:
+            if self.encodeToUtf8 and isinstance(jsonData, bytes):
+                jsonData = jsonData.decode("utf-8")
+
+            dataDict = json.loads(jsonData)
+            sd = SensorData()
+            sd.__dict__.update(dataDict)
+            return sd
+        except Exception as e:
+            self.logger.error("Failed to convert JSON to SensorData: %s", e)
+            return None
+
+    # ---- SystemPerformanceData conversions ----
+    def systemPerformanceDataToJson(self, sysPerfData: SystemPerformanceData) -> str:
+        if sysPerfData is None:
+            return None
+        try:
+            jsonData = json.dumps(sysPerfData.__dict__)
+            return jsonData.encode("utf-8") if self.encodeToUtf8 else jsonData
+        except Exception as e:
+            self.logger.error("Failed to convert SystemPerformanceData to JSON: %s", e)
+            return None
+
+    def jsonToSystemPerformanceData(self, jsonData: str) -> SystemPerformanceData:
+        if not jsonData:
+            return None
+        try:
+            if self.encodeToUtf8 and isinstance(jsonData, bytes):
+                jsonData = jsonData.decode("utf-8")
+
+            dataDict = json.loads(jsonData)
+            spd = SystemPerformanceData()
+            spd.__dict__.update(dataDict)
+            return spd
+        except Exception as e:
+            self.logger.error("Failed to convert JSON to SystemPerformanceData: %s", e)
+            return None
